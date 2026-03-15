41dc98851634cb5764b7f80eb3beaf73395d02be
Carla Leticia Ribeiro Franca <carlainlondon@protonmail.com>
2026-03-15 20:06:45 -0300

    chore: estrutura inicial do projeto Spring Boot

---
+++ src/main/java/br/com/clrf/TopFilmesApplication.java
@@ -0,0 +1,10 @@
+package br.com.clrf;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopFilmesApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(TopFilmesApplication.class, args);
+    }
+}
